20b395a6e6fc389500a2cb5feb410edb1f559cff
dsliu <fysoso2006@163.com>
2016-05-18 17:10:04 +0800

    订单信息提交

---
+++ vip/vip-core-lib/src/main/java/com/jianfei/core/dto/OrderAddInfoDto.java
@@ -0,0 +1,13 @@
+package com.jianfei.core.dto;
+
+/**
+ * 用户提交订单信息
+ *
+ * @version 1.0.0
+ * @Description:
+ * @author: liu.dongsong@jianfeitech.com
+ * @date: 2016/5/18 15:59
+ */
+public class OrderAddInfoDto {
+
+}
