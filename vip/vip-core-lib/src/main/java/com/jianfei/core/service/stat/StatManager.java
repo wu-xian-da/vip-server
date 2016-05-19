6bd101391ea3f921e2f29048617273686921657f
leoliuliu <programboy@126.com>
2016-05-19 18:38:20 +0800

    add stat module

---
+++ vip/vip-core-lib/src/main/java/com/jianfei/core/service/stat/StatManager.java
@@ -0,0 +1,21 @@
+/**
+ * @项目名:vip
+ * @版本信息:1.0
+ * @date:2016年5月19日-上午10:49:24
+ * Copyright (c) 2016建飞科联公司-版权所有
+ *
+ */
+package com.jianfei.core.service.stat;
+
+/**
+ *
+ * @Description: TODO
+ * @author: li.binbin@jianfeitech.com 
+ * @date: 2016年5月19日 上午10:49:24 
+ * 
+ * @version 1.0.0
+ *
+ */
+public interface StatManager {
+
+}
