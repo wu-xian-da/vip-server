4f387c3944a24acae600d9dfc460561f400b94b9
li.binbin <li.binbin@jianfeitech.com>
2016-05-19 09:42:03 +0800

    更新代码

--- vip/vip-webapp/src/main/java/com/jianfei/controller/UserController.java
+++ vip/vip-webapp/src/main/java/com/jianfei/controller/UserController.java
@@ -18,7 +18,6 @@ import org.apache.shiro.crypto.hash.SimpleHash;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -57,6 +56,7 @@ public class UserController extends BaseController {
 		return "user/Syuser";
 	}
 
+	@SuppressWarnings("rawtypes")
 	@RequestMapping(value = "/list", method = RequestMethod.POST)
 	@ResponseBody
 	public Grid list(
