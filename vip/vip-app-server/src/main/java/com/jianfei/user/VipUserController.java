53a02508676407b3959cb76d7c534468bc9385b5
dsliu <fysoso2006@163.com>
2016-05-24 17:54:35 +0800

    用户相关接口实现

--- vip/vip-app-server/src/main/java/com/jianfei/user/VipUserController.java
+++ vip/vip-app-server/src/main/java/com/jianfei/user/VipUserController.java
@@ -1,7 +1,18 @@
 package com.jianfei.user;
 
+import com.jianfei.common.BaseMsgInfo;
+import com.jianfei.core.bean.AppCustomer;
+import com.jianfei.core.bean.SysViproom;
+import com.jianfei.core.service.base.impl.AriPortServiceImpl;
+import com.jianfei.core.service.thirdpart.impl.MsgInfoManagerImpl;
+import com.jianfei.core.service.user.impl.VipUserManagerImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Vip用户Controller
@@ -12,10 +23,50 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @date: 2016/5/17 15:51
  */
 @Controller
-@RequestMapping(value = "vip")
+@RequestMapping(value = "vipUser")
 public class VipUserController {
 
+    @Autowired
+    private VipUserManagerImpl vipUserManager;
 
+    /**
+     * VIP 用户登录
+     * @return
+     */
+    @RequestMapping(value = "/login")
+    @ResponseBody
+    public BaseMsgInfo vipRoomInfo(@RequestParam(value = "phone", required = true) String phone,
+                                   @RequestParam(value = "code", required = true) String code
+    ) {
+     boolean validate= vipUserManager.validateLoginCode(phone,code);
+     if (validate){
+         return BaseMsgInfo.success("");
+     }else {
+         return BaseMsgInfo.fail("");
+     }
+    }
 
+    /**
+     * VIP 获取用户信息
+     * @return
+     */
+    @RequestMapping(value = "/getUser")
+    @ResponseBody
+    public BaseMsgInfo getUser(@RequestParam(value = "phone", required = true) String phone
+    ) {
+        AppCustomer  appCustomer= vipUserManager.getUser(phone);
+        return BaseMsgInfo.success(appCustomer);
+    }
+
+    /**
+     * VIP 用户信息更新
+     * @return
+     */
+    @RequestMapping(value = "/updateUserInfo")
+    @ResponseBody
+    public BaseMsgInfo updateUserInfo(AppCustomer appCustomer
+    ) {
+        return BaseMsgInfo.success(vipUserManager.updateUser(appCustomer));
+    }
 
 }
