171a01d1dc7a77ed41f886ca1b6816645d2b230d
guojian <1552869197@qq.com>
2016-05-26 11:06:53 +0800

    用户反馈

--- vip/vip-webapp/src/main/java/com/jianfei/controller/AppUserFeedbackController.java
+++ vip/vip-webapp/src/main/java/com/jianfei/controller/AppUserFeedbackController.java
@@ -8,12 +8,17 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.github.pagehelper.PageInfo;
+import com.jianfei.core.bean.AppCustomer;
 import com.jianfei.core.bean.AppUserFeedback;
+import com.jianfei.core.common.utils.MessageDto;
+import com.jianfei.core.service.base.AppCustomerManager;
 import com.jianfei.core.service.base.impl.AppUserFeedbackImpl;
 /**
  * 用户反馈管理controller
@@ -25,6 +30,8 @@ import com.jianfei.core.service.base.impl.AppUserFeedbackImpl;
 public class AppUserFeedbackController {
 	@Autowired
 	private AppUserFeedbackImpl appUserFeedbackImpl;
+	@Autowired
+	private AppCustomerManager appCustomerManager;
 	/**
 	 * 跳转到用户反馈管理页面
 	 * @return
@@ -35,7 +42,43 @@ public class AppUserFeedbackController {
 	}
 	
 	
+	/**
+	 * 
+	 * 跳转到用户反馈详情页面
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("gotoFeedBackDetailView")
+	public String gotoFeedBackDetailView(String userId,Model model){
+		MessageDto<AppCustomer> messageDto = appCustomerManager
+				.selectByPrimaryKey(userId);
+		if (messageDto.isOk()) {
+			model.addAttribute("customer", messageDto.getData());
+		}
+		appCustomerManager.batchDealMsg(userId, model);
+		return "feedback/feedBackDetail";
+	}
 	
+	
+	/**
+	 * 跳转到用户反馈处理页面
+	 * @param userId
+	 * @param feedbackId
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("goHandFeedbackView")
+	public String goHandFeedbackView(String userId,String feedbackId,String feedbackContent,Model model){
+		MessageDto<AppCustomer> messageDto = appCustomerManager
+				.selectByPrimaryKey(userId);
+		if (messageDto.isOk()) {
+			model.addAttribute("customer", messageDto.getData());
+		}
+		model.addAttribute("feedbackContent", feedbackContent);
+		model.addAttribute("feedbackId", feedbackId);
+		appCustomerManager.batchDealMsg(userId, model);
+		return "feedback/handfeedbackView";
+	}
 	
 	/**
 	 * 用户反馈信息分页查询
@@ -62,10 +105,10 @@ public class AppUserFeedbackController {
 			appUserFeedback.setFormatFeedbackTime(formatDate(appUserFeedback.getFeedbacTime()));
 			if(appUserFeedback.getFeedbackState() == 0){//反馈信息未处理
 				appUserFeedback.setFeedbackStateName("未处理");
-				appUserFeedback.setOpr("<button class='btn'><a href='returnOrderDetailInfoByOrderId?orderId=1001'>处理</a></button>");
+				appUserFeedback.setOpr("<button class='btn'><a href='goHandFeedbackView?userId="+appUserFeedback.getUserId()+"&feedbackId="+appUserFeedback.getId()+"&feedbackContent="+appUserFeedback.getFeedbackContent()+"'>处理</a></button>");
 			}else if(appUserFeedback.getFeedbackState() == 1){//反馈信息已处理
 				appUserFeedback.setFeedbackStateName("已处理");
-				appUserFeedback.setOpr("<button class='btn'><a href='returnOrderDetailInfoByOrderId?orderId=1001'>查看</a></button><button class='btn' style='background-color:red' onclick='delFeedBackInfo("+appUserFeedback.getId()+")'>删除</button>");
+				appUserFeedback.setOpr("<button class='btn'><a href='gotoFeedBackDetailView?userId="+appUserFeedback.getUserId()+"'>查看</a></button><button class='btn' style='background-color:red' onclick='delFeedBackInfo("+appUserFeedback.getId()+")'>删除</button>");
 			}
 		}
 		resMap.put("total", page.getTotal());
@@ -87,6 +130,20 @@ public class AppUserFeedbackController {
 		resMap.put("result", 1);//1代表成功
 		return resMap;
 	}
+	
+	/**
+	 * 更新用户反馈的状态
+	 * @param appUserFeedback
+	 * @return
+	 */
+	@RequestMapping(value="updateFeedbackState",method=RequestMethod.POST)
+	public String updateFeedbackState(AppUserFeedback appUserFeedback){
+		System.out.println("feedbackid="+appUserFeedback.getId());
+		appUserFeedbackImpl.updateFeedbackInfoById(appUserFeedback.getId()+"");
+		return "redirect:goFeedbackView";
+	}
+	
+	
 	//时间格式化
 	public String formatDate(Date date){
 		 SimpleDateFormat sFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
