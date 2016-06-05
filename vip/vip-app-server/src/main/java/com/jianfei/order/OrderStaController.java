be86ee88cfa65e7f0054d414854ba3fbf85f69dc
dsliu <fysoso2006@163.com>
2016-06-06 00:53:22 +0800

    订单统计接口提交

--- vip/vip-app-server/src/main/java/com/jianfei/order/OrderStaController.java
+++ vip/vip-app-server/src/main/java/com/jianfei/order/OrderStaController.java
@@ -1,5 +1,21 @@
 package com.jianfei.order;
 
+import com.github.pagehelper.PageInfo;
+import com.jianfei.core.bean.AppOrderArchive;
+import com.jianfei.core.bean.SysAirport;
+import com.jianfei.core.bean.SysViproom;
+import com.jianfei.core.common.enu.PayType;
+import com.jianfei.core.common.utils.PageDto;
+import com.jianfei.core.dto.BaseMsgInfo;
+import com.jianfei.core.service.order.impl.OrderManagerImpl;
+import com.jianfei.core.service.stat.impl.StatManagerImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * TODO
  *
@@ -8,5 +24,28 @@ package com.jianfei.order;
  * @author: liu.dongsong@jianfeitech.com
  * @date: 2016/5/27 11:10
  */
+@Controller
+@RequestMapping(value = "orderSta")
 public class OrderStaController {
+
+    @Autowired
+    private StatManagerImpl statManager;
+
+    /**
+     * 分页获取
+     * @param pageDto
+     * @param uno
+     * @return
+     */
+    @RequestMapping(value = "/pageSaleVipCards")
+    @ResponseBody
+    public BaseMsgInfo pageSaleVipCards(PageDto pageDto, @RequestParam(value = "uno", required = true) String uno
+    ) {
+        PageInfo<AppOrderArchive> pageInfo= statManager.pageOrderStatByUserId(pageDto,uno);
+        return BaseMsgInfo.success(pageInfo);
+    }
+
+
 }
+
+
--- vip/vip-core-lib/src/main/java/com/jianfei/core/common/utils/IdGen.java
+++ vip/vip-core-lib/src/main/java/com/jianfei/core/common/utils/IdGen.java
@@ -2,6 +2,7 @@ package com.jianfei.core.common.utils;
 
 import java.io.Serializable;
 import java.security.SecureRandom;
+import java.util.Date;
 import java.util.UUID;
 
 import org.apache.shiro.session.Session;
@@ -23,10 +24,15 @@ public class IdGen implements SessionIdGenerator {
 	private static SecureRandom random = new SecureRandom();
 
 	/**
-	 * 封装JDK自带的UUID, 通过Random数字生成, 中间无-分割.
+	 * 参数machineId：是集群时的机器代码，可以1-9任意。部署时，分别为部署的项目手动修改该值，以确保集群的多台机器在系统时间上不一致的问题（毫无疑问每台机器的毫秒数基本上不一致）。
+	 参数System.currentTimeMillis():这是java里面的获取1970年到目前的毫秒数,是一个13位数的数字，与Date.getTime()函数的结果一样，比如1378049585093。经过研究，在2013年，前三位是137，在2023年是168，到2033年才199.所以，我决定第一位数字1可以去掉，不要占位置了。可以肯定绝大多数系统用不了10年20年。这样，参数2就变成了12位数的数字，加上参数1machineId才13位数。
+	 参数System.nanoTime()：这是java里面的取纳秒数，经过深入研究，在同一毫秒内，位置7,8,9这三个数字是会变化的。所以决定截取这三个数字出来拼接成一个16位数的订单号。
+	 总结：理论上此方案在同一秒内，可以应对1000*1000个订单号，但是经过测试，在每秒并发2000的时候，还是会出现2-10个重复
 	 */
 	public static String uuid() {
-		return UUID.randomUUID().toString().replaceAll("-", "");
+		return "0"+
+				(System.currentTimeMillis()+"").substring(1)+
+				(System.nanoTime()+"").substring(7,10);
 	}
 
 	/**
