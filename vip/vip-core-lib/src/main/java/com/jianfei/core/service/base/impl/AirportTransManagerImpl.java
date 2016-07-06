8222a314c784ba2b3455e4c835e321c0c4ac516c
dsliu <fysoso2006@163.com>
2016-07-06 10:04:39 +0800

    增加用户判断的接口

--- vip/vip-core-lib/src/main/java/com/jianfei/core/service/base/impl/AirportTransManagerImpl.java
+++ vip/vip-core-lib/src/main/java/com/jianfei/core/service/base/impl/AirportTransManagerImpl.java
@@ -1,10 +1,13 @@
 package com.jianfei.core.service.base.impl;
 
 import com.jianfei.core.bean.AppAirportTrans;
+import com.jianfei.core.bean.AppCustomer;
 import com.jianfei.core.common.utils.IdGen;
+import com.jianfei.core.common.utils.StringUtils;
 import com.jianfei.core.dto.BaseMsgInfo;
 import com.jianfei.core.mapper.AppAirportTransMapper;
 import com.jianfei.core.service.base.AirportTransManager;
+import com.jianfei.core.service.user.VipUserManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +26,9 @@ public class AirportTransManagerImpl implements AirportTransManager {
 
     @Autowired
     private AppAirportTransMapper appAirportTransMapper;
+
+    @Autowired
+    private VipUserManager vipUserManager;
     /**
      * 添加接送机相关信息
      *
@@ -31,6 +37,12 @@ public class AirportTransManagerImpl implements AirportTransManager {
      */
     @Override
     public BaseMsgInfo addAirportTransInfo(AppAirportTrans airportTrans) {
+        //查找下相关用户
+        AppCustomer customer= vipUserManager.getUser(airportTrans.getPhone());
+        if (customer==null || StringUtils.isBlank(customer.getCustomerId())){
+            BaseMsgInfo.msgFail("用户不存在，接送机信息添加失败");
+        }
+        airportTrans.setName(customer.getCustomerName());
         airportTrans.setId(IdGen.uuid());
         airportTrans.setCreateDate(new Date());
         int i = appAirportTransMapper.insertSelective(airportTrans);
