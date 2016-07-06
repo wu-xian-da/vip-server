437dbe725c7ff0051ff5829ff03ab9bb7efb63f3
dsliu <fysoso2006@163.com>
2016-07-06 15:16:51 +0800

    修改紧急退卡方式 消息类型错误

--- vip/vip-core-lib/src/main/java/com/jianfei/core/service/order/impl/OrderManagerImpl.java
+++ vip/vip-core-lib/src/main/java/com/jianfei/core/service/order/impl/OrderManagerImpl.java
@@ -465,13 +465,13 @@ public class OrderManagerImpl implements OrderManager {
         msgBuilder.setMsgBody(object.toJSONString());
         //添加订单状态为已退款
         if (StringUtils.isNotBlank(appCardBack.getAgreementUrl())) {
-            //更改订单状态为已退款 和申请方式为
+            //紧急退卡 更改订单状态为已退款 和申请方式为紧急
             orders.setOrderState(VipOrderState.ALREADY_REFUND.getName());
-            msgBuilder.setMsgType(MsgType.BACK_CARD_APPLY.getName());
+            msgBuilder.setMsgType(MsgType.RIGHT_BACK_CARD.getName());
         } else {
             //审核通过
             orders.setOrderState(VipOrderState.AUDIT_PASS.getName());
-            msgBuilder.setMsgType(MsgType.RIGHT_BACK_CARD.getName());
+            msgBuilder.setMsgType(MsgType.BACK_CARD_APPLY.getName());
         }
         log.info("更改VIP卡状态");
         AppVipcard vipcard=new AppVipcard();
