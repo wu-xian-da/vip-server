c226fd92cb85acf16ee64af51cbd38047ec878d0
dsliu <fysoso2006@163.com>
2016-07-20 14:19:39 +0800

    修改最新的退卡规则

--- vip/vip-core-lib/src/main/java/com/jianfei/core/service/order/impl/OrderManagerImpl.java
+++ vip/vip-core-lib/src/main/java/com/jianfei/core/service/order/impl/OrderManagerImpl.java
@@ -361,7 +361,7 @@ public class OrderManagerImpl implements OrderManager {
     @Override
     public BaseMsgInfo getVipCardUseAndOrder(String phone, String code, String vipCardNo) {
         //1、校验用户和手机验证码
-        boolean flag = true;//validateCodeManager.validateSendCode(phone, MsgType.SELECT, code);
+        boolean flag = validateCodeManager.validateSendCode(phone, MsgType.SELECT, code);
         if (!flag)
             return new BaseMsgInfo().setCode(-1).setMsg("验证码校验失败");
         //2、查询用户信息和订单信息
