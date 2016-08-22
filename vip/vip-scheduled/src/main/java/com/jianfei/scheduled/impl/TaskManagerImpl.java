30763059ae2d4502998aea16046cbbf053333fe4
li.binbin <li.binbin@jianfeitech.com>
2016-08-22 11:44:53 +0800

    屏蔽定时核销接口

--- vip/vip-scheduled/src/main/java/com/jianfei/scheduled/impl/TaskManagerImpl.java
+++ vip/vip-scheduled/src/main/java/com/jianfei/scheduled/impl/TaskManagerImpl.java
@@ -86,53 +86,36 @@ public class TaskManagerImpl implements ITaskManager {
 	 * 
 	 * @see com.jianfei.scheduled.ITaskManager#checkinDataSchedule()
 	 */
-	@Scheduled(fixedRate = 600000, initialDelay = 600000)
 	public void checkinDataSchedule() {
-		logger.info("<<<<<<获取空港核销数据>>>>>>");
-		try {
-			AirportEasyUseInfo aeInfo = airportEasyManager.getVipCardUseInfo();
-			if (aeInfo != null) {
-				airportEasyManager.sendConfirmInfo(aeInfo.getBatchNo());
-				List<AppConsume> clist = aeInfo.getConsumeList();
-				if (clist != null) {
-					int size = aeInfo.getConsumeList().size();
-					for (int i = 0; i < size; i++) {
-						AppConsume appConsume = clist.get(i);
-						consumeManager.addConsume(appConsume);
-
-						// 判断是否是第一次使用，如果是，redis计时加1，更新卡的第一次使用时间和有效时间，如果不是,redis计时累计加1
-						try {
-							firstServiceHandle(appConsume);
-						} catch (Exception e) {
-							logger.error("对卡信息使用次数统计异常:" + e.getMessage());
-						}
-						return;
-					}
-				}
-
-			}
-
-		} catch (UnrecoverableKeyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (KeyManagementException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (KeyStoreException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		logger.info("<<<<<<获取空港核销数据>>>>>>");
+//		try {
+//			AirportEasyUseInfo aeInfo = airportEasyManager.getVipCardUseInfo();
+//			if (aeInfo != null) {
+//				airportEasyManager.sendConfirmInfo(aeInfo.getBatchNo());
+//				List<AppConsume> clist = aeInfo.getConsumeList();
+//				if (clist != null) {
+//					int size = aeInfo.getConsumeList().size();
+//					for (int i = 0; i < size; i++) {
+//						AppConsume appConsume = clist.get(i);
+//						consumeManager.addConsume(appConsume);
+//
+//						// 判断是否是第一次使用，如果是，redis计时加1，更新卡的第一次使用时间和有效时间，如果不是,redis计时累计加1
+//						try {
+//							firstServiceHandle(appConsume);
+//						} catch (Exception e) {
+//							logger.error("对卡信息使用次数统计异常:" + e.getMessage());
+//						}
+//						return;
+//					}
+//				}
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		} 
 	}
 
-	// 每1秒执行一次
 	/**
-	 * 从消息队列中拉消息
+	 * 从消息队列中拉消息，每1秒执行一次
 	 */
 	@Scheduled(fixedRate = 1000)
 	public void pullSmsMessage() {
