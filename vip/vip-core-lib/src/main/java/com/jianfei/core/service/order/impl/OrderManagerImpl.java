d31efb69a55c148365f279938722f1a7417fafd8
guojian <1552869197@qq.com>
2017-03-17 11:29:48 +0800

    订单补录测试用例

--- vip/vip-core-lib/src/main/java/com/jianfei/core/service/order/impl/OrderManagerImpl.java
+++ vip/vip-core-lib/src/main/java/com/jianfei/core/service/order/impl/OrderManagerImpl.java
@@ -43,6 +43,8 @@ import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -170,6 +172,82 @@ public class OrderManagerImpl implements OrderManager {
 		appOrderCard.setCardType(vipCard.getCardType());
 		appOrderCardMapper.insert(appOrderCard);
 
+		addInfoDto.setOrderId(orders.getOrderId());
+		addInfoDto.setMoney(vipCard.getInitMoney());
+		// **日志记录（正常）
+		SmartLog.info(ModuleType.ORDER_MODULE.getName(), customer.getPhone(),
+				"【订单模块-订单添加】，订单编号：" + orders.getOrderId() + "，用户手机号：" + customer.getPhone() + "，用户姓名："
+						+ customer.getCustomerName() + "，操作员编号：" + user.getId() + "，操作员姓名：" + user.getName()
+						+ "，操作内容：【订单状态:未支付】，操作结果：【成功】");
+		return BaseMsgInfo.success(addInfoDto);
+
+	}
+	
+	/**
+	 * 自定义方法，用于后期补录订单信息，不走后台
+	 * @param addInfoDto
+	 * @return
+	 * @throws InvocationTargetException
+	 * @throws IllegalAccessException
+	 * @throws ParseException 
+	 */
+	public BaseMsgInfo addOrderAndUserInfoByOutLine(OrderAddInfoDto addInfoDto)
+			throws InvocationTargetException, IllegalAccessException, ParseException {
+		// 根据查询VIP号查询卡片信息
+		AppVipcard vipCard = vipCardManager.getVipCardByNo(addInfoDto.getVipCardNo());
+		if (vipCard == null) {
+			return new BaseMsgInfo().setCode(-1).setMsg("VIP卡号错误");
+		} 
+		// 销售员信息
+		User user = saleUserManager.getSaleUser(addInfoDto.getUno());
+		if (user == null || StringUtils.isBlank(user.getName())) {
+			return BaseMsgInfo.msgFail("人员工号不存在");
+		}
+		
+		// 1、添加或修改用户信息
+		AppCustomer customer = new AppCustomer();
+		
+		BeanUtils.copyProperties(customer, addInfoDto);
+		customer.setSex(1);
+		vipUserManager.addORUpdateUser(customer);
+
+		// 2、添加订单信息
+		AppOrders orders = new AppOrders();
+		BeanUtils.copyProperties(orders, addInfoDto);
+		orders.setSaleNo(addInfoDto.getUno());
+		orders.setSaleName(user.getName());
+		orders.setCustomerId(customer.getCustomerId());
+		orders.setPayMoney(vipCard.getInitMoney());
+		orders.setOrderId(IdGen.uuid());
+		orders.setOrderTime(new Date());
+		orders.setRemark1("VIP卡");
+		orders.setOrderState(VipOrderState.ALREADY_PAY.getName());//订单状态-已支付
+		orders.setOrderTime(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2017-03-16 10:00:00"));
+		orders.setPayTime(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2017-03-16 10:01:00"));//支付时间
+		orders.setPayType(PayType.CASHPAY.getName());//支付方式-现金
+		orders.setDtflag(StateType.EXIST.getName());
+		orders.setInvoiceFlag(0);
+		appOrdersMapper.insertSelective(orders);
+
+		// 3、卡表及订单卡表
+		vipCard.setCustomerId(customer.getCustomerId());
+		vipCardManager.updateVipCard(vipCard);
+		AppOrderCard appOrderCard = new AppOrderCard();
+		appOrderCard.setId(IdGen.uuid());
+		appOrderCard.setOrderId(orders.getOrderId());
+		appOrderCard.setCardNo(addInfoDto.getVipCardNo());
+		appOrderCard.setCardNum(1);
+		appOrderCard.setInitMoney(vipCard.getInitMoney());
+		appOrderCard.setCardType(vipCard.getCardType());
+		appOrderCardMapper.insert(appOrderCard);
+		
+		//4 将卡的状态改为绑定成功未激活1
+		AppVipcard vipcard = new AppVipcard();
+		vipcard.setCardState(VipCardState.ACTIVE.getName());
+		vipCardManager.updateByPrimaryKeySelective(vipcard);
+		
+		
+		
 		addInfoDto.setOrderId(orders.getOrderId());
 		addInfoDto.setMoney(vipCard.getInitMoney());
 		// **日志记录（正常）
--- vip/vip-core-lib/src/test/java/com/jianfei/core/service/order/OrderManagerTest.java
+++ vip/vip-core-lib/src/test/java/com/jianfei/core/service/order/OrderManagerTest.java
@@ -1,6 +1,8 @@
 package com.jianfei.core.service.order;
 
-import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,14 +12,83 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.jianfei.core.dto.OrderAddInfoDto;
+import com.jianfei.core.service.order.impl.OrderManagerImpl;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:persistence.xml" })
 @Transactional
 public class OrderManagerTest {
 	@Autowired
-	private OrderManager orderManager;
-	
+	private OrderManagerImpl orderManagerImpl;
+
+	public List<OrderAddInfoDto> returnOrderInfos() {
+		OrderAddInfoDto addInfoDto1 = new OrderAddInfoDto();
+		addInfoDto1.setCustomerName("李向前");
+		addInfoDto1.setPhone("13934057666");
+		addInfoDto1.setCustomerIdenti("140423197207020017");
+		addInfoDto1.setUno("019");
+		addInfoDto1.setVipCardNo("86857118715");
+		addInfoDto1.setAirportId("0468218270952502");
+
+		OrderAddInfoDto addInfoDto2 = new OrderAddInfoDto();
+		addInfoDto2.setCustomerName("贾平");
+		addInfoDto2.setPhone("13935203343");
+		addInfoDto2.setCustomerIdenti("140211196301011335");
+		addInfoDto2.setUno("019");
+		addInfoDto2.setVipCardNo("86818598042");
+		addInfoDto2.setAirportId("0468218270952502");
+
+		OrderAddInfoDto addInfoDto3 = new OrderAddInfoDto();
+		addInfoDto3.setCustomerName("亢波");
+		addInfoDto3.setPhone("15198874936");
+		addInfoDto3.setCustomerIdenti("142225198802281834");
+		addInfoDto3.setUno("029");
+		addInfoDto3.setVipCardNo("86884121602");
+		addInfoDto3.setAirportId("0468218270952502");
+
+		OrderAddInfoDto addInfoDto4 = new OrderAddInfoDto();
+		addInfoDto4.setCustomerName("刘阳");
+		addInfoDto4.setPhone("18234040388");
+		addInfoDto4.setCustomerIdenti("142730199210191539");
+		addInfoDto4.setUno("015");
+		addInfoDto4.setVipCardNo("86870469997");
+		addInfoDto4.setAirportId("0468218270952502");
+
+		OrderAddInfoDto addInfoDto5 = new OrderAddInfoDto();
+		addInfoDto5.setCustomerName("罗彝科");
+		addInfoDto5.setPhone("18758204992");
+		addInfoDto5.setCustomerIdenti("532922198606040517");
+		addInfoDto5.setUno("030");
+		addInfoDto5.setVipCardNo("86878761669");
+		addInfoDto5.setAirportId("0468045723714265");
+
+		OrderAddInfoDto addInfoDto6 = new OrderAddInfoDto();
+		addInfoDto6.setCustomerName("冷德涛");
+		addInfoDto6.setPhone("13942662371");
+		addInfoDto6.setCustomerIdenti("210225197405180150");
+		addInfoDto6.setUno("031");
+		addInfoDto6.setVipCardNo("86854676438");
+		addInfoDto6.setAirportId("0468045723714265");
+
+		OrderAddInfoDto addInfoDto7 = new OrderAddInfoDto();
+		addInfoDto7.setCustomerName("刘怡");
+		addInfoDto7.setPhone("13942030409");
+		addInfoDto7.setCustomerIdenti("210203197311230120");
+		addInfoDto7.setUno("027");
+		addInfoDto7.setVipCardNo("86877296264");
+		addInfoDto7.setAirportId("0468045723714265");
+
+		List<OrderAddInfoDto> list = new ArrayList<>();
+		list.add(addInfoDto1);
+		list.add(addInfoDto2);
+		list.add(addInfoDto3);
+		list.add(addInfoDto4);
+		list.add(addInfoDto5);
+		list.add(addInfoDto6);
+		list.add(addInfoDto7);
+		return list;
+	}
+
 	/**
 	 * 添加订单信息
 	 * 
@@ -25,17 +96,12 @@ public class OrderManagerTest {
 	@Test
 	@Rollback(false)
 	public void testAddOrderAndUserInfo() {
-		OrderAddInfoDto addInfoDto = new OrderAddInfoDto();
-		addInfoDto.setCustomerName("武俊德");
-		addInfoDto.setPhone("13835033266");
-		addInfoDto.setCustomerIdenti("142228196612100019");
-		addInfoDto.setUno("012");
-		addInfoDto.setVipCardNo("86870855294");
-		addInfoDto.setAirportId("0468218270952502");
+		List<OrderAddInfoDto> list = returnOrderInfos();
 		try {
-			orderManager.addOrderAndUserInfo(addInfoDto);
-		} catch (InvocationTargetException | IllegalAccessException e) {
-			// TODO Auto-generated catch block
+			for (OrderAddInfoDto orderAddInfoDto : list) {
+				orderManagerImpl.addOrderAndUserInfoByOutLine(orderAddInfoDto);
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
